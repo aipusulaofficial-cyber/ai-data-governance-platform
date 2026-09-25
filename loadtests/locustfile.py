ff4e49b4e83193945de3b7e55c26d967c12f3d90
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-09-25 18:23:45 +0300

    style: align governance sources with Ruff formatting

--- loadtests/locustfile.py
+++ loadtests/locustfile.py
@@ -4,7 +4,6 @@ from locust import HttpUser, task, between
 class APIUser(HttpUser):
     wait_time = between(0.1, 0.5)
     @task
-
     def domain(self):
         self.client.post(
             "/v1/governance",
